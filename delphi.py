888e4a93cac187488f789230db1a50f17c7b690f
Essel Jimmy Kodwo <jimmy.essel@kibo.school>
2023-03-31 21:03:59 +0000

    first commit

---
+++ delphi.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr
+import pyttsx3
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+engine.say('I am your alexa')
+engine.say('What can I do for you?')
+try:
+    with sr.Microphone() as source:
+        print("listening...")
+        voice = listener.listen(source)
+        command = listener.recognize_google(voice)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
+except:
+    pass
